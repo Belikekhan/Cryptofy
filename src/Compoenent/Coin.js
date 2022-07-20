342e0fcb38aa93f8ecabdff057f4467e898d6259
Belikekhan <109422803+Belikekhan@users.noreply.github.com>
2022-07-20 15:53:24 +0530

    jg
    mhv

--- src/Compoenent/Coin.js
+++ src/Compoenent/Coin.js
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { SingleCoin } from "../config/api";
 import Coininfo from "./Coininfo";
+import { Line } from "react-chartjs-2";
 
 import { CryptoState } from "./Cryptoccnt";
 const Coin = () => {
@@ -21,39 +22,63 @@ const Coin = () => {
   }, []);
 
   return (
-    <div>
-      <div>
-        {/* sidebar */}
-        <img src={coin?.image.large} height="200px" />
-        <Typography variant="h3">{coin?.name}</Typography>
-      </div>
-      <div>
-        <span style={{ display: "flex" }}>
-          {" "}
-          <Typography variant="h5">Rank:</Typography>
-          &nbsp; &nbsp;
-          <Typography variant="h5">{coin?.market_cap_rank}</Typography>
-        </span>
-
-        <span style={{ display: "flex" }}>
-          {" "}
-          <Typography variant="h5">Price:</Typography>
-          &nbsp; &nbsp;
-          <Typography variant="h5">
-            {coin?.market_data.current_price.aed}
+    <div style={{ display: "flex" }}>
+      <div
+        style={{ borderRight: "1px gold solid", width: "50vw", height: "90vh" }}
+      >
+        <div
+          style={{
+            marginTop: "20px",
+            marginLeft: "50px",
+          }}
+        >
+          {/* sidebar */}
+          <img src={coin?.image.large} height="200px" />
+          <Typography variant="h3" style={{ marginTop: "20px" }}>
+            {coin?.name}
           </Typography>
-        </span>
+        </div>
+        <div
+          style={{
+            marginTop: "20px",
+            marginLeft: "50px",
+            gap: "20px",
+          }}
+        >
+          <span style={{ display: "flex" }}>
+            {" "}
+            <Typography variant="h5">Rank:</Typography>
+            &nbsp; &nbsp;
+            <Typography variant="h5">{coin?.market_cap_rank}</Typography>
+          </span>
 
-        <span style={{ display: "flex" }}>
-          {" "}
-          <Typography variant="h5">Price:</Typography>
-          &nbsp; &nbsp;
-          <Typography variant="h5">
-            {coin?.market_data.market_cap.aed}
-          </Typography>
-        </span>
+          <span style={{ display: "flex" }}>
+            {" "}
+            <Typography variant="h5">Price:</Typography>
+            &nbsp; &nbsp;
+            <Typography variant="h5">
+              {coin?.market_data.current_price.aed}
+            </Typography>
+          </span>
+
+          <span style={{ display: "flex" }}>
+            {" "}
+            <Typography variant="h5">Price:</Typography>
+            &nbsp; &nbsp;
+            <Typography variant="h5">
+              {coin?.market_data.market_cap.aed}
+            </Typography>
+          </span>
+          {/* <span style={{ display: "flex" }}>
+            {" "}
+            <Typography variant="h5">Description:</Typography>
+            &nbsp; &nbsp;
+            <Typography>{coin?.description.ar.split(0, 100)}</Typography>
+          </span> */}
+        </div>
       </div>
       {/* chart  */}
+      {/* <Line />; */}
       <Coininfo coin={coin} />
     </div>
   );
--- src/Compoenent/Coininfo.js
+++ src/Compoenent/Coininfo.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { HistoricalChart } from "../config/api";
+import { CryptoState } from "./Cryptoccnt";
+import axios from "axios";
+import { Line } from "react-chartjs-2";
 
-const Coininfo = () => {
-  return <div>Coininfo</div>;
+const Coininfo = ({ coin }) => {
+  console.log(coin);
+  const [history, sethistory] = useState();
+  const [days, setday] = useState(1);
+
+  const { Curn } = CryptoState();
+
+  const FethHistory = async () => {
+    const { data } = axios.get(
+      `https://api.coingecko.com/api/v3/coins/${coin.id}/market_chart?vs_currency=${Curn}&days=${days}`
+        .then((data) => sethistory(data))
+        .console.log(history)
+    );
+  };
+  useEffect(() => {
+    FethHistory();
+  }, [Curn, days]);
+
+  return <div>{/* <Line data={label} /> */}</div>;
 };
 
 export default Coininfo;
